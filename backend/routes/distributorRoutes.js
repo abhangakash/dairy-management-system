eb0ddf284ec4afe6cf53ef8927a0995c6250e3cf
abhangakash <157141882+abhangakash@users.noreply.github.com>
2026-03-29 11:09:32 +0000

    routes for distributor page removed

--- backend/routes/distributorRoutes.js
+++ backend/routes/distributorRoutes.js
@@ -6,12 +6,10 @@ const {
   getDistributors,
   updateDistributor,
   deleteDistributor,
-  toggleStatus,
 } = require("../controllers/distributorController");
 
 router.post("/", authMiddleware, createDistributor);
 router.get("/", authMiddleware, getDistributors);
-router.patch("/status/:id", authMiddleware, toggleStatus);
 router.put("/:id", authMiddleware, updateDistributor);
 router.delete("/:id", authMiddleware, deleteDistributor);
 
