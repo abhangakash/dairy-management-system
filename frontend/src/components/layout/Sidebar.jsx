a4ac518b29ab780f9b02bf7ccef1aad0299dd5a8
abhangakash <157141882+abhangakash@users.noreply.github.com>
2026-04-05 03:44:25 +0000

    Production Module added

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -21,6 +21,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
     { to: "/workers", label: "Workers", icon: <Users size={20} /> },
     { to: "/product-entries", label: "Product Entry", icon: <Users size={20} /> },
     { to: "/raw-materials", label: "Raw Material", icon: <Users size={20} /> },
+    { to: "/production", label: "Production", icon: <Users size={20} /> },
     { label: "Partnerships", isHeader: true },
     { to: "/distributors", label: "Distributors", icon: <Truck size={20} /> },
     { to: "/partners", label: "Partners", icon: <Handshake size={20} /> },
